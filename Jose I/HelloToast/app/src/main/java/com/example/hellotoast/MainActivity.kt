eabee0834a4737d000beab5196e52cd1882cccde
Illera1999 <illerarodriguez1999@gmail.com>
2022-10-14 22:36:45 +0200

    Primeros pasos con Android

---
+++ Jose I/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hellotoast
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {
+
+
+    }
+    fun countUp(view: View) {}
+}
