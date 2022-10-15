52d24ea25e8188a40384f687d2d039c0dacf6ab8
Illera1999 <illerarodriguez1999@gmail.com>
2022-10-16 01:07:38 +0200

    1.3 Aumento del dado: Funcional

--- Jose I/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Jose I/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.diceroller
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import java.util.logging.Logger.global
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var num: Int=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,19 +26,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollAumenta() {
         val resultImage: ImageView = findViewById(R.id.dice_image)
-
-        if (resultImage.drawable.equals(R.drawable.dice_1)) {
-            resultImage.setImageResource(R.drawable.dice_2)
+        val drawableImage = when (num) {
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            5 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
         }
-        /*} else if (resultImage.drawable.equals(R.drawable.dice_2)){
-            resultImage.setImageResource(R.drawable.dice_3)
-        } else if (resultImage.drawable.equals(R.drawable.dice_3)){
-            resultImage.setImageResource(R.drawable.dice_4)
-        } else if (resultImage.drawable.equals(R.drawable.dice_4)){
-            resultImage.setImageResource(R.drawable.dice_5)
-        } else {
-            resultImage.setImageResource(R.drawable.dice_6)
-        }*/
+        if(num<=6){num++}
+        resultImage.setImageResource(drawableImage)
+
 
     }
 
@@ -40,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val resultImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = (1..6).random()
+        num = randomInt.toInt()
         val drawableImage = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
