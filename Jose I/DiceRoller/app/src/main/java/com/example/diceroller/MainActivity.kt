4c36ca6e3118f243832e4b4cb66af2b635011f6d
Illera1999 <illerarodriguez1999@gmail.com>
2022-10-16 20:23:26 +0200

    1.3 DiceRollerFinal

--- Jose I/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Jose I/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
@@ -14,10 +15,17 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     var num: Int=1
+    var num2: Int=1
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         val upButton: Button = findViewById(R.id.up_button)
         rollButton.setOnClickListener{ rollDice() }
@@ -25,7 +33,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollAumenta() {
-        val resultImage: ImageView = findViewById(R.id.dice_image)
         val drawableImage = when (num) {
             1 -> R.drawable.dice_2
             2 -> R.drawable.dice_3
@@ -35,16 +42,26 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         if(num<=6){num++}
-        resultImage.setImageResource(drawableImage)
+        diceImage.setImageResource(drawableImage)
+        val drawableImage2 = when (num2) {
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            5 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+        }
+        if(num2<=6){num2++}
+        diceImage2.setImageResource(drawableImage2)
 
 
     }
 
     private fun rollDice(){
-        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultImage: ImageView = findViewById(R.id.dice_image)
+        // val resultImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = (1..6).random()
-        num = randomInt.toInt()
+        val randomInt2 = (1..6).random()
+        num = randomInt
         val drawableImage = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -53,6 +70,16 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        resultImage.setImageResource(drawableImage)
+        val drawableImage2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        num2 = randomInt2
+        diceImage.setImageResource(drawableImage)
+        diceImage2.setImageResource(drawableImage2)
     }
 }
