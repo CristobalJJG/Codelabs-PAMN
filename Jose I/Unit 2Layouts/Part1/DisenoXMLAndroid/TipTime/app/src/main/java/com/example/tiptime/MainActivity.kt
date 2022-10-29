f1b84af033989d33ab593505b84e83821c33c0d7
Illera1999 <illerarodriguez1999@gmail.com>
2022-10-29 18:34:27 +0100

    Mejorando el tiptimer

--- Jose I/Unit 2Layouts/Part1/DisenoXMLAndroid/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Jose I/Unit 2Layouts/Part1/DisenoXMLAndroid/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -10,25 +10,30 @@ import kotlin.math.cos
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
     fun calculateTip() {
-        val cost = binding.costOfService.text.toString().toDouble()
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-        var tip = cost * tipPercentage
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp) {
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
             tip = ceil(tip)
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
